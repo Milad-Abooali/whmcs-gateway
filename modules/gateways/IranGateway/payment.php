f2d6686c1b732748a00d1f947f37818fd76662f4
Milad Abooali <m.abooali@hotmail.com>
2022-02-12 14:50:50 +0300

    Meta

--- modules/gateways/IranGateway/payment.php
+++ modules/gateways/IranGateway/payment.php
@@ -21,3 +21,11 @@ global $CONFIG;
 $cb_gw_name    = 'IranGateway';
 $cb_output     = ['POST'=>$_POST,'GET'=>$_GET];
 $action 	   = isset($_GET['a']) ? $_GET['a'] : false;
+
+$root_path     = '../../../';
+$includes_path = '../../../includes/';
+include($root_path.((file_exists($root_path.'init.php'))?'init.php':'dbconnect.php'));
+include($includes_path.'functions.php');
+include($includes_path.'gatewayfunctions.php');
+include($includes_path.'invoicefunctions.php');
+$modules    = getGatewayVariables($cb_gw_name);
